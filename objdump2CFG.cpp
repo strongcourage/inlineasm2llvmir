e29e93cbd2e03cdb025801de0db2c65548611982
Manh Dung Nguyen <nguyenmanhdung1710@gmail.com>
2016-05-12 11:02:18 +0800

    Implement unconditional branch.

--- objdump2CFG.cpp
+++ objdump2CFG.cpp
@@ -6,6 +6,8 @@
 #include <functional> 
 #include <cctype>
 #include <locale>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -22,7 +24,7 @@ struct InfoInst {
     string keyInst;
     string addressInst;
     string opcodesInst;
-    pair <string, int> offsetInst;
+    pair <string, string> offsetInst; //<next_function, offset_in_hex>
 };
 
 // trim from start
@@ -42,6 +44,19 @@ static inline string trim(string str) {
     return ltrim(rtrim(str));
 }
 
+// find position of first occurrence of special character
+static inline int first_position(string str, const char c) {
+    int pos = -1;
+    for (int i=0; i<str.size(); i++) {
+        if (str.at(i) == c) {
+            pos = i;
+            break;
+        }
+    }
+    return pos;
+}
+
+
 static void objdump2CFG(string fileName) {
     ifstream ifs(fileName);
     string newFileName = "new_" + fileName;
@@ -49,62 +64,86 @@ static void objdump2CFG(string fileName) {
     string line;
     int keywordsSize = sizeof(keywordsInsts)/sizeof(string);
 
+    map<string, vector<InfoInst> > mapFuncInst; // <function_name, list_of_instructions_of_function>
+    vector<InfoInst> vectorInst;
+
     // open and read objdump input file
     if (ifs.is_open()) {
         while(getline(ifs, line)) {
-            // cout << line << endl;
             string::size_type pos_keyword = string::npos;
-            string::size_type pos_colon = string::npos;
-            string::size_type pos_not_colon_opcodes = string::npos;
-            string keyword;
-            string address;
-            string next_func;
-            string att_insts;
-            int offset = 0;
+            string keyword, address, opcodes, next_func, att_insts, offset;
+            string insert_line;
 
-            for (int i=0; i<line.size(); i++) {
-                if (line.at(i) == ':') {
-                    // cout << "pos_colon: " << i << endl;
-                    pos_colon = i;
-                    address = trim(line.substr(0, pos_colon));
-                }
+            int pos_colon = first_position(line, ':');
+            if (pos_colon >= 0) {
+                // cout << "pos_colon: " << pos_colon << endl;
+                address = trim(line.substr(0, pos_colon));
             }
 
             for (int i=0; i<keywordsSize; i++) {
                 pos_keyword = line.find(keywordsInsts[i]);
                 // cout << "pos_keyword: " << pos_keyword << endl;
-                // cout << "line size: " << line.size() << endl;
                 keyword = keywordsInsts[i];
                 
                 
                 if (pos_keyword != string::npos && pos_colon != string::npos) {
-                    string extract_line = "ins " + trim(line.substr(0, pos_keyword-1)); // line.resize(pos_keyword);
-                    cout << "extract_line: " << extract_line << endl;
-                    ofs << extract_line << endl;
+                    insert_line = "ins " + trim(line.substr(0, pos_keyword-1)); // line.resize(pos_keyword);
+                    ofs << insert_line << endl;
 
-                    string opcodes = trim(extract_line.substr(pos_colon+1, pos_keyword));
-                    cout << "opcodes: " << opcodes << endl;
-
+                    opcodes = trim(insert_line.substr(pos_colon+3, pos_keyword-pos_colon-3));
+                    // cout << "opcodes: " << opcodes << endl;
                     att_insts = trim(line.substr(pos_keyword+keyword.size()+1, line.size()).c_str());
-                    cout << "att_insts: " << att_insts << endl;
 
-                    // unconditional branch
+                    // update struct InfoInst of each instruction
+                    InfoInst current_inst = { keyword, address, opcodes, make_pair("", "")}; 
+                    vectorInst.push_back(current_inst);
+
+                    // unconditional branch, for example: jle    28 <main+0x28>; jmpq   33 <main+0x33>
                     if (keyword == "jmpq" || keyword == "jle") {
+                        int pos_1 = first_position(att_insts, '<');
+                        int pos_2 = first_position(att_insts, '+');
+                        int pos_3 = first_position(att_insts, '>');
+                        if (pos_1 >= 0 && pos_2 >= 0) {
+                            next_func = att_insts.substr(pos_1+1, pos_2-pos_1-1);
+                            current_inst.offsetInst.first = next_func;
+                            // cout << "next_func: " << next_func << endl;
+                            string offset = att_insts.substr(pos_2+3, pos_3-pos_2-3);
+                            current_inst.offsetInst.second = offset;
+                            // cout << "offset: " << offset << endl;
 
+                            // insert a true branch at the end of this instruction
+                            string trueBr = " trueBr=" + offset;
+                            insert_line += trueBr;
+                        }
                     }
 
                     // conditional branch
 
-                    // update struct InfoInst of each instruction
-                    InfoInst current_inst = { keyword, address, opcodes, make_pair("", 0)}; 
-                    cout << "instruction: ";
-                    cout << current_inst.keyInst << " " << current_inst.addressInst << " " << current_inst.opcodesInst 
-                        << " " <<  current_inst.offsetInst.first << " " << current_inst.offsetInst.second << endl;
 
+                    // print infos of the current instruction                    
+                    cout << "instruction: ";
+                    cout << current_inst.keyInst << " / " << current_inst.addressInst << " / " << current_inst.opcodesInst 
+                         << " / " <<  current_inst.offsetInst.first << " / " << current_inst.offsetInst.second << endl;
                 }
+            }
+
+            // write this line to the new objdump file
+            ofs << insert_line << endl;
+        }
+        // TODO: only consider main function. Need to find all existed functions
+        mapFuncInst.insert(make_pair("main", vectorInst)); 
+
+        // print all instructions which belongs to a function
+        for (map<string, vector<InfoInst>>::iterator map_it=mapFuncInst.begin(); map_it!=mapFuncInst.end(); ++map_it) {
+            cout << map_it->first << endl;
+            for (vector<InfoInst>::iterator vec_it=map_it->second.begin(); vec_it!=map_it->second.end(); ++vec_it) {
+                cout << "instruction: "; 
+                cout << vec_it->keyInst << " / " << vec_it->addressInst << " / " << vec_it->opcodesInst 
+                     << " / " <<  vec_it->offsetInst.first << " / " << vec_it->offsetInst.second << endl;
             }
         }
 
+        // close file
         ifs.close();
     } else {
         cout << "Unable to open objdump input file"; 
